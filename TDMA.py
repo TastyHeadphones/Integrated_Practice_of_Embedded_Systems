74c6bb322e51bd8e43d9542405fbd7c981acf82b
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2021-11-06 23:02:46 +0800

    初步模块提交

---
+++ TDMA.py
@@ -0,0 +1,23 @@
+import time
+
+class TDMA():
+    def __init__(self,timeInfo) -> None:
+
+        self.order = 0
+        self.timeInfo = timeInfo
+        self.TimeInterval = self.getTimeInterval()
+
+    def getTimeInterval():
+        pass
+    
+    def instance(self):
+        self.order += 1       
+        return self.order
+
+    def isTimeVaild(order):
+        pass
+    
+    
+
+
+
